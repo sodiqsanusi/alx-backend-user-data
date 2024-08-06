1db0b38a00bf4d6f7dd0568562722d7819066801
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-08-05 22:15:26 -0700

    task: trying out the basic auth task

---
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Documentations on the initialisation of the API
+"""
+from flask import Blueprint
+from api.v1.views.index import *
+from api.v1.views.users import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+User.load_from_file()
